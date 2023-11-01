6424f1dba6aecb8795c82a36402662923c9fbbd4
Antony Chuvirau <anton_chuvirau@icloud.com>
2023-11-01 16:18:56 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -8,13 +8,25 @@ const slideshow = new Slideshow(slides);
 // document.querySelector('.slides-nav__item--next').addEventListener('click', () => slideshow.next());
 // Initialize the GSAP Observer plugin
 Observer.create({
-    type: 'wheel,touch,pointer',
+    type: 'touch,pointer',
     onDown: () => slideshow.prev(),
     onUp: () => slideshow.next(),
     // invert the mouse wheel delta
     wheelSpeed: -1,
     tolerance: 10
 });
+
+var Auto;
+
+function startAuto() {
+  clearInterval(Auto);    
+  Auto = setInterval(function(){ 
+    element = slideshow.next();
+    setActive(element);
+  }, 3000);
+}
+
+startAuto()
 
 // Preload all images. Once all images are preloaded, remove the 'loading' class from the body.
 preloadImages('.slide__img').then(() => document.body.classList.remove('loading'));
